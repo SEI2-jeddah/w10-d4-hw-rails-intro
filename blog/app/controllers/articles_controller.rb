9fed0277d4bd7d59076e966de34edac62315c6d5
amal-nj <amal.alnajjar96@gmail.com>
2019-12-10 14:10:18 +0300

    Done Part 2 & 3

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "amal", password: "123", except: [:index, :show]    
     def index
         @articles = Article.all
     end
