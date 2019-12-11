39539161ca48cf8c2a370f7377cd7c4c758664aa
Fah11ad <falqurashi11@gmail.com>
2019-12-11 10:41:47 +0300

    part2-and-part3

--- blog/test/controllers/welcome_controller_test.rb
+++ blog/test/controllers/welcome_controller_test.rb
@@ -6,4 +6,24 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get create" do
+    get welcome_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get welcome_show_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get welcome_update_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get welcome_new_url
+    assert_response :success
+  end
+
 end
