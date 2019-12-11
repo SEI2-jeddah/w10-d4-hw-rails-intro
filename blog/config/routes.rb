e552cda00eb4a4a49cf1f86f99d1e375a5498806
abdullah alsharif <abdullahalsharif@abdullahs-MacBook-Air.local>
2019-12-11 11:17:39 +0300

    Three parts are completed and submited here...Directed by AAA

---
+++ blog/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # get 'welcome/index'
+  # get 'welcome/create'
+  # get 'welcome/show'
+  # get 'welcome/update'
+  # get 'welcome/new'
+  resources :articles do 
+      resources :comments
+  end
+  resources :welcome 
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
